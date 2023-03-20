37253959bc97b3b08211dd1e14ef2095800cee4c
Cameron Ellis <cameronellis46@gmail.com>
2023-03-20 18:55:53 +0000

    fixed a lot of things

--- main.c
+++ main.c
@@ -24,6 +24,7 @@ long long timestamp;
 long buffer[1];
 long long time_buffer[1];
 sem_t mySem;
+int printPipe[2];
 
 long prevLoc=0, prevTS=0;
 long nextLoc=0, nextTS = 0;
@@ -32,6 +33,7 @@ long buttonTS = 0;
 
 void* Thread0(void*);
 void* Child_Thread(void*);
+void* Thread1(void*);
 
 
 int main(void){
@@ -41,6 +43,12 @@ int main(void){
 	pthread_t thrd0;
 
 	pthread_create(&thrd0, NULL, &Thread0, NULL);
+
+	pipe(printPipe);
+
+	pthread_t thrd1;
+
+	pthread_create(&thrd1, NULL, &Thread1, NULL);
 	
 	int pfd = open("/tmp/N_pipe1", O_RDONLY);
 
@@ -50,12 +58,10 @@ int main(void){
 		// Read from N_Pipe1 and store the values in buffer sizeof(char)
 		//Store the reading in location[0]
 		read(pfd, &location[0], sizeof(char));
-		buffer[0] = (long)location[0];
 		//get time convert it to milliseconds
 		struct timespec tspec;
         clock_gettime(CLOCK_MONOTONIC, &tspec);
 		timestamp = (long long)((tspec.tv_sec * 1000) + (tspec.tv_nsec / 1000000));
-		time_buffer[0] = timestamp;
 	}
 
 	// Join thread -- won't reach here anyways!
@@ -68,20 +74,16 @@ int main(void){
 
 void* Thread0(void* ptr) {
 
-	// Thread for child thread
-	pthread_t childthrd[5];
     // Read from N_Pipe2 and store the values in time_buffer sizeof(long)
 	int pfd = open("/tmp/N_pipe2", O_RDONLY);
-	// Index of child thread bounded by 5
-	int i = 0;
 
 	while(1){
+		// Thread for child thread
+		pthread_t childthrd;
 		read(pfd, &buttonTS, sizeof(long));
-		prevTS = (long)time_buffer[0];
-		prevLoc = buffer[0];
-		i = i % 5;
-		pthread_create(&childthrd[i], NULL, &Child_Thread, NULL);
-		i++;
+		prevTS = (long)timestamp;
+		prevLoc = (long)location[0];
+		pthread_create(&childthrd, NULL, &Child_Thread, NULL);
 	}
 
 	pthread_exit(NULL);
@@ -93,14 +95,21 @@ void* Thread0(void* ptr) {
  * 	is going to use the slope intercept form to interpolate the value.
  */
 void* Child_Thread(void* ptr){
+	long buttonBuf = buttonTS;
 	while (1)
 	{
-		if(location[0] != prevLoc) {
+		if((long)location[0] != prevLoc) {
 			break;
 		}
 	}
-
 	nextLoc = (long)location[0];
+	
+	while (1)
+	{
+		if(prevTS != timestamp) {
+			break;
+		}
+	}
 	nextTS 	= (long)timestamp;
 
 
@@ -109,7 +118,7 @@ void* Child_Thread(void* ptr){
 	long double deltaLoc 	= (long double)(nextLoc - prevLoc);
 	long double deltaTS		= (long double)(nextTS - prevTS);
 	long double slope 		= (long double)(deltaLoc/deltaTS);
-	long double tmp 		= ((long double)slope)*((long double)(buttonTS - prevTS));
+	long double tmp 		= ((long double)slope)*((long double)(buttonBuf - prevTS));
 	long double y0 			= (long double)tmp + (long double)prevLoc;
 
 	// Now print the information on the screen. We need to be careful in printing
@@ -118,18 +127,36 @@ void* Child_Thread(void* ptr){
 	// the access so that while only one thread printing the info on the screen
 	// the rest of the threads will wait for it to finish its printing job. We
 	// need a semaphore for that.
+	long double output[6];
+	output[0] = (long double)prevTS;
+	output[1] = (long double)prevLoc;
+	output[2] = (long double)buttonBuf;
+	output[3] = (long double)y0;
+	output[4] = (long double)nextTS;
+	output[5] = (long double)nextLoc;
 	sem_wait(&mySem);
-	printf("\nInterpolation Results\n");
-	printf("\n---------------------\n");
-	printf("GPS Previous TS = %lf \t GPS Previous Location = %lf\n", 
-			(long double)prevTS, (long double)prevLoc);
-	printf("Button TS = %lf \t Button Location = %lf\n", \
-			(long double)buttonTS, (long double)y0);
-	printf("GPS Next TS = %lf \t GPS Next Location = %lf\n", \
-			(long double)nextTS, (long double)nextLoc);
-	printf("\n\n");
+	write(printPipe[1], output, sizeof(long double) * 6);
 	sem_post(&mySem);
 
+
+	pthread_exit(NULL);
+}
+
+
+void* Thread1(void* ptr) {
+	long double print[6];
+	while(1){
+		read(printPipe[0], print, sizeof(long double) * 6);
+		printf("\nInterpolation Results\n");
+		printf("\n---------------------\n");
+		printf("GPS Previous TS = %lf \t GPS Previous Location = %lf\n", 
+			(long double)print[0], (long double)print[1]);
+		printf("Button TS = %lf \t Button Location = %lf\n", \
+			(long double)print[2], (long double)print[3]);
+		printf("GPS Next TS = %lf \t GPS Next Location = %lf\n", \
+			(long double)print[4], (long double)print[5]);
+		printf("\n\n");
+	}
 
 	pthread_exit(NULL);
 }
